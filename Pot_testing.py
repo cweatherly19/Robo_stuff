b6f95a8982ac849906432b3935991048351b7117
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2019-06-04 08:55:08 -0500

    Create Pot_testing.py

---
+++ Pot_testing.py
@@ -0,0 +1,8 @@
+import RoboPiLib_pwm as RPL #to pull all files needed to run the motors
+RPL.RoboPiInit("/dev/ttyAMA0", 115200) #connect to RoboPi
+
+motor_pin = 8
+
+while True:
+  pot_value = RPL.analogRead(motor_pin)
+  print pot_value
