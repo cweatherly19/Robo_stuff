648305381d633a924045cc7b602f9ad31d9eba5f
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2019-06-04 09:24:55 -0500

    Update Pot_testing.py

--- Pot_testing.py
+++ Pot_testing.py
@@ -2,9 +2,9 @@ import RoboPiLib_pwm as RPL #to pull all files needed to run the motors
 RPL.RoboPiInit("/dev/ttyAMA0", 115200) #connect to RoboPi
 import time
 
-motor_pin = 8
+pot_pin = 8
 
 while True:
-  pot_value = RPL.analogRead(motor_pin)
+  pot_value = RPL.analogRead(pot_pin)
   print pot_value
   time.sleep(0.5)
