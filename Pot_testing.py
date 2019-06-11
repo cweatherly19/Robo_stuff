5d5b57db1e45239c04576ba4fbdbec761647dedc
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2019-06-11 12:45:56 -0500

    Update Pot_testing.py

--- Pot_testing.py
+++ Pot_testing.py
@@ -2,9 +2,20 @@ import RoboPiLib_pwm as RPL #to pull all files needed to run the motors
 RPL.RoboPiInit("/dev/ttyAMA0", 115200) #connect to RoboPi
 import time
 
-pot_pin = 6
+elbow_range = 775 #range of read values by elbow POT
+shoulder_range = 825 #range of read values by shoulder POT
+
+pot_pin_elbow = 6
+pot_pin_shoulder = 7
+
+print "Output given in degrees"
+print "Elbow pin:", pot_pin_elbow
+print "Shoulder pin:", pot_pin_shoulder
+time.sleep(5)
 
 while True:
   pot_value = RPL.analogRead(pot_pin)
-  print(pot_value)
+  print(pot_value_shoulder * 270 / shoulder_range)
+  time.sleep(0.05)
+  print(pot_value_elbow * 270 / elbow_range)
   time.sleep(0.05)
